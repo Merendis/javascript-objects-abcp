65aacf5e565a020042d8bad6f98a7be54b9ae9ae
Meredith D Doucette <meredith.doucette@gmail.com>
2018-01-20 23:00:31 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {};
+
+function updatePlaylist(playlistobj, artist, song){
+  Object.assign({}, playlistobj, {[`${artist}`]: `${song}`} )
+  playlistobj;
+}
+
+updatePlaylist(playlist, "Phil Ochs", "Slowdrive");
+
+function removeFromPlaylist(playlistobj, artist){
+  return delete playlistobj[`${artist}`]
+  return playlistobj;
+}
+
